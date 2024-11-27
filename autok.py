b8b08d7832a180d662044675a89ab8a6e69b6e7f
Blaj3n <benceulrich@gmail.com>
2024-11-27 19:27:03 +0100

    3. feladat legelső kocsi leadott jelzései

--- autok.py
+++ autok.py
@@ -15,3 +15,14 @@ print(autok)
 feladatok(2)
 
 print(f"Az utolsó jeladás időpontja {autok[-1][1]}:{autok[-1][2]}, a jármű rendszáma {autok[-1][0]}")
+
+feladatok(3)
+
+elso_jarmu = autok[0][0]
+print(f"Az első jármű: {elso_jarmu}")
+print("Jeladásainak időpontjai: ", end="")
+
+for auto in autok:
+    if elso_jarmu == auto[0]:
+        print(f"{auto[1]}:{auto[2]}", end=" ")
+print()
